0376047420382e2a1c1ce497d1ac4b5caf58e933
Cristiano Rodrigues <crmprojetosweb@gmail.com>
2022-08-02 22:59:28 -0300

    Layers

---
+++ backend1/src/main/java/com/fabricadesitesalterbeach/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.fabricadesitesalterbeach.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fabricadesitesalterbeach.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
